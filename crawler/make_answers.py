abbbb7783aaef92296e8c7a4548bdd4425a5d607
Nattawut Phetmak <neizod@gmail.com>
2014-11-27 20:24:24 +0700

    since sql update is too long, merge make-lang into insert.

--- crawler/make_answers.py
+++ crawler/make_answers.py
@@ -4,25 +4,38 @@ import os
 import sys
 import json
 
-from dry import metadata, iter_id_io
+from glob import iglob
+from dry import metadata, lang_name, iter_id_io
 
 
-if len(sys.argv) != 2:
-    exit('usage: ./make_users.py [year]')
+def get_ext(file):
+    return os.path.splitext(file)[1].lower().lstrip('.')
+
+def get_lang(exts):
+    for ext in exts:
+        if ext in lang_name:
+            return lang_name[ext]
+
+
+if not 2 < len(sys.argv) <= 3:
+    exit('usage: ./make_users.py [year] [contest]')
 year = int(sys.argv[1])
+cont = sys.argv[2] if sys.argv[2] else None
 
 
 base = set()
 more = set()
 for contest in metadata[year]:
     adaptor = base if 'Qualification' in contest['name'] else more
-    for user in json.load(open(filename)):
+    for user in json.load(open('../data/{}.json'.format(contest['id']))):
         adaptor.add(user['n'])
 if more - base:
     raise NameError(more - base)
 
 answers = []
 for contest in metadata[year]:
+    if cont is not None and cont.lower() not in contest['name'].lower():
+        continue
     filename = '../data/{}.json'.format(contest['id'])
     if not os.path.isfile(filename):
         exit('data for year {} does not exist.'.format(year))
@@ -32,8 +45,14 @@ for contest in metadata[year]:
         for a, s, o, (num, io) in zip(answer['att'], answer['ss'], answer['oa'], id_io):
             if a == 0:
                 continue
-            answers += ['  ({}, {}, {}, {}, {})'.format(us, num, io, a, s)]
+            if s != -1:
+                files = '../source/{}/{}-{}/*'.format(num, io, answer['n'])
+                exts = {get_ext(file) for file in iglob(files)}
+                lang = get_lang(exts)
+            else:
+                lang = None
+            answers += ['  ({}, {}, {}, {}, {}, {!r})'.format(us, num, io, a, s, lang)]
 
-print('INSERT INTO answers (user_id, problem_id, hardness, attempts, submit_time)\nVALUES')
+print('INSERT INTO answers (user_id, problem_id, hardness, attempts, submit_time, lang)\nVALUES')
 print(',\n'.join(answers))
 print(';')
