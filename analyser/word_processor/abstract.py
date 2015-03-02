569ca7c9869ff495e39d01a0c5449ccfcd36bb03
Nattawut Phetmak <neizod@gmail.com>
2015-03-03 01:06:23 +0700

    simple implement count lines of block (func/class).

--- analyser/word_processor/abstract.py
+++ analyser/word_processor/abstract.py
@@ -66,3 +66,87 @@ class WordProcessor(object):
         intercode = self.strip_noise(intercode)
         intercode = self.strip_numeric(intercode)
         return Counter(intercode.split())
+
+
+    def get_line_of_blocks(self, sourcecode):
+        if self.name == 'Python':
+            return self._python_line_of_blocks(sourcecode)
+        elif self.name in ['Java', 'C#']:
+            return self._java_line_of_blocks(sourcecode)
+        else:
+            return self._cpp_line_of_blocks(sourcecode)
+
+
+    def _python_line_of_blocks(self, sourcecode):
+        lines = sourcecode.splitlines()
+        in_block = False
+        blocks = []
+        for index, line in enumerate(lines):
+            if line.startswith(' ') or line.startswith('\t'):
+                if not in_block:
+                    while True:
+                        index -= 1
+                        block_name = lines[index].strip()
+                        if block_name:
+                            break
+                    blocks += [[block_name, 1]]
+                in_block = True
+            elif line.strip():
+                if in_block:
+                    while not lines[index-1].strip():
+                        index -= 1
+                        blocks[-1][1] -= 1
+                in_block = False
+            if in_block:
+                blocks[-1][1] += 1
+        return blocks
+
+
+    def _cpp_line_of_blocks(self, sourcecode):
+        sourcecode = self.strip_string(sourcecode)
+        sourcecode = self.strip_comment(sourcecode)
+        sym_open = '{'
+        sym_close = '}'
+        depth = 0
+        blocks = []
+        for index, char in enumerate(sourcecode):
+            if char == sym_open:
+                if depth == 0:
+                    before = index
+                    while True:
+                        before = max(0, sourcecode.rfind('\n', 0, before))
+                        block_name = sourcecode[before:index].strip()
+                        if block_name or before == 0:
+                            break
+                    blocks += [[block_name, 0]]
+                depth += 1
+            elif char == sym_close:
+                depth -= 1
+            if depth > 0 and char == '\n':
+                blocks[-1][1] += 1
+        return [[it, length] for it, length in blocks if it.find('=') == -1]
+
+
+    def _java_line_of_blocks(self, sourcecode):
+        sourcecode = self.strip_string(sourcecode)
+        sourcecode = self.strip_comment(sourcecode)
+        sym_open = '{'
+        sym_close = '}'
+        depth = 0
+        blocks = []
+        for index, char in enumerate(sourcecode):
+            if char == sym_open:
+                if depth == 1:
+                    before = index
+                    while True:
+                        before = max(0, sourcecode.rfind('\n', 0, before))
+                        block_name = sourcecode[before:index].strip()
+                        if block_name:
+                            break
+                    blocks += [[block_name, 0]]
+                depth += 1
+            elif char == sym_close:
+                depth -= 1
+            if depth > 1 and char == '\n':
+                blocks[-1][1] += 1
+        return [[it, length] for it, length in blocks if it.find('=') == -1]
