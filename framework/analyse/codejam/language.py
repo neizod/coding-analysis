fff42bdd2d6800a2927352a695a695cc6536feac
Nattawut Phetmak <neizod@gmail.com>
2015-04-24 15:50:09 +0700

    simple implement of extract/analyse language.

---
+++ framework/analyse/codejam/language.py
@@ -0,0 +1,38 @@
+import os
+import json
+import statistics as stat
+from collections import Counter, defaultdict
+
+from ..._utils import datapath
+from ..._utils import word_processor
+
+
+def repr_or_na(data):
+    return repr(data) if data is not None else 'NA'
+
+
+def summary_row(answer):
+    return '{} {} {} {}\n'.format(
+            answer['pid'],
+            answer['io'],
+            answer['screen_name'],
+            repr_or_na(answer['language']))
+
+
+def calculate_identifier_length(year, **kwargs):
+    os.makedirs(datapath('result'), exist_ok=True)
+    with open(datapath('result', 'language.txt'), 'w') as file:
+        file.write('pid io screen_name language\n')
+        for answer in json.load(open(datapath('extract', 'language.json'))):
+            file.write(summary_row(answer))
+
+
+def update_parser(subparsers):
+    subparser = subparsers.add_parser('language', description='''
+        This method will analyse language used in each subbmited code.''')
+    subparser.add_argument('year', type=int, help='''
+        year of a contest.''')
+    # TODO force
+    subparser.add_argument('-q', '--quiet', action='store_true', help='''
+        run the script quietly.''')
+    subparser.set_defaults(function=calculate_identifier_length)
---
+++ framework/extract/codejam/language.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+from ..._utils import datapath, log, iter_submission, readsource
+from ..._utils import word_processor
+
+
+def extract_identifier(year, force=False, quiet=False, **kwargs):
+    os.makedirs(datapath('extract'), exist_ok=True)
+    output_file = 'extract/language.json'
+    if not force and os.path.isfile(datapath(output_file)):
+        return
+    extracted_data = []
+    for pid, io, screen_name in iter_submission(year):
+        directory = 'source/{}/{}/{}/'.format(pid, io, screen_name)
+        quiet or log(directory)
+        languages = set()
+        for filename in os.listdir(datapath(directory)):
+            if not os.path.isfile(datapath(directory, filename)):
+                continue
+            _, ext = os.path.splitext(datapath(directory, filename))
+            try:
+                prolang = word_processor.select(ext)
+            except KeyError:
+                continue
+            languages |= {prolang.name}
+        quiet or log('  done\n')
+        extracted_data += [{
+            'pid': pid,
+            'io': io,
+            'screen_name': screen_name,
+            'language': languages.pop() if len(languages) == 1 else None,
+        }]
+    with open(datapath(output_file), 'w') as file:
+        json.dump(extracted_data, file, indent=2)
+
+
+
+def update_parser(subparsers):
+    subparser = subparsers.add_parser('language', description='''
+        This method will extract name of programming language used
+        in each submission.''')
+    subparser.add_argument('year', type=int, help='''
+        year of a contest.''')
+    subparser.add_argument('-f', '--force', action='store_true', help='''
+        force override output.''')
+    subparser.add_argument('-q', '--quiet', action='store_true', help='''
+        run script quietly.''')
+    subparser.set_defaults(function=extract_identifier)
