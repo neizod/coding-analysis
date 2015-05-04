ad5dea0ce33a9b24cd6e4832f5ee2b79f10d0955
Nattawut Phetmak <neizod@gmail.com>
2015-05-04 07:23:48 +0700

    make barebone github w/ init metadata.

---
+++ framework/github/extract/__init__.py
@@ -0,0 +1,6 @@
+from framework._utils import hook_submodules
+
+
+def update_parser(subparsers):
+    subparser = subparsers.add_parser('extract')
+    hook_submodules(subparser, __file__, __name__)
