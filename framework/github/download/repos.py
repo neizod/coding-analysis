733f118f01282ae7fca60b01963c4caa4aa10ee4
Nattawut Phetmak <neizod@gmail.com>
2015-05-22 23:27:34 +0700

    feature: download only some repos written in specific language.

--- framework/github/download/repos.py
+++ framework/github/download/repos.py
@@ -7,12 +7,14 @@ from framework._utils import FunctionHook
 class GitHubDownloadRepos(FunctionHook):
     ''' download/update git repositories from GitHub.'''
 
-    def main(self, **_):
+    def main(self, only_lang=None, **_):
         import git
         from framework._utils.misc import datapath
         from framework.github._helper import make_url, iter_repos
         os.makedirs(datapath('github', 'repos'), exist_ok=True)
-        for _, repo in iter_repos():
+        for lang, repo in iter_repos():
+            if only_lang is not None and lang.lower() != only_lang.lower():
+                continue
             directory = datapath('github', 'repos', repo['name'])
             if not os.path.isdir(directory):
                 logging.info('downloading: %s', repo['name'])
