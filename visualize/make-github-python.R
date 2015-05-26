d4de065ce0b2c655d2bbbf657fa4e3d90c5d005b
Nattawut Phetmak <neizod@gmail.com>
2015-05-26 14:26:04 +0700

    analyse: github python repos.

---
+++ visualize/make-github-python.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+
+python.data = read.table('../data/github/result/compatibility-python.txt', header=T)
+python.data$date = as.Date(python.data$date)
+
+compat.ok = c('django', 'httpie', 'ipython', 'powerline', 'requests',
+              'shadowsocks', 'YouCompleteMe', 'youtube-dl', 'thefuck',
+              'legit', 'python-patterns')
+
+compat.swing = c('compose', 'pyspider', 'salt')
+
+compat.almost = c('flask', 'tornado', 'scrapy', 'sentry', 'scikit-learn',
+                  'fabric', 'beets', 'ajenti', 'Flashlight', 'celery',
+                  'pandas', 'boto')
+
+compat.noway = c('ansible', 'bup', 'Dshell', 'reddit', 'huxley', 'sshuttle')
+
+compat.all = c(compat.ok, compat.swing, compat.almost, compat.noway)
+
+python.data = python.data[python.data$repo %in% compat.all,]
+
+png('gh-py-all.png', width=600, height=450)
+ggplot(python.data, aes(x=date, y=py3/files, group=repo)) +
+    geom_line() +
+    theme(legend.position='none') +
+    theme_bw()
+dev.off()
